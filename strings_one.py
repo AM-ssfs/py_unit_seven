597f6911604836d8ebc99623b7aebe2fc6912c17
AM-ssfs <menscad@ssfs.org>
2021-11-08 08:50:43 -0500

    strings and stuff

--- strings_one.py
+++ strings_one.py
@@ -1,16 +1,31 @@
 def half_slice(word):
-    pass
+    num = int(len(word)/2)
+    front = word[:num]
+    back = word[num:]
+    word2 = back + front
+    return word2
 
 
 def no_first_last(str):
-    pass
+    word = ""
+    for x in range(1, len(str)-1):
+        word += str[x]
+    return word
 
 
 def longest(phrase):
-    pass
+    words = phrase.split(" ")
+    big = 0
+    for x in range(len(words)):
+        long = len(words[x])
+        if big < long:
+            big = long
+    return big
+
 
 
 def title_case(sentence):
     pass
 
 
+longest("ada asdaf ad")
--- upper_lower.py
+++ upper_lower.py
@@ -1,7 +1,22 @@
+def get_input():
+    return str(input("Say something: "))
+
+
+def write(word):
+    print(word.upper())
+    print(word.lower())
+
+
+def letters_in(word):
+    for x in range(len(word)):
+        print(word[x])
 
 
 def main():
-    pass
+    word = get_input()
+    write(word)
+    letters_in(word)
+
 
 if __name__ == '__main__':
     main()
