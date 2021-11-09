94aee6abe73bbbea209232cff99b2ab5a8bd09ed
AM-ssfs <menscad@ssfs.org>
2021-11-09 10:02:48 -0500

    strings and stuff

--- strings_two.py
+++ strings_two.py
@@ -1,6 +1,20 @@
 def four_letters(phrase):
-    pass
+    abc = phrase.split(" ")
+    for x in range(len(abc)):
+        if len(abc[x]) == 4:
+            abc[x] = "\"#$%@\""
+    phrase = " ".join(abc)
+    return phrase
 
 
 def bubble_sort(words):
     pass
+
+
+# print(four_letters("all of the four letter words in this sentence have been replaced with \"#$%@\""))
+
+
+def word_score(word_list):
+    letters = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+    letters = letters.split(" ")
+    print(letters)
